d1cab1b1e3ffa0c8bf6f0ce715588650acd3f3d5
G.Batault <g.batault@citegestion.fr>
2020-10-06 14:06:53 +0200

    toast

--- src/components/toast.rs
+++ src/components/toast.rs
@@ -35,36 +35,48 @@ pub fn view(model: &Model) -> Vec<Node<Msg>> {
         St::Left => percent(50),
         St::Width => rem(20),
         St::MarginLeft => rem(-10),
-        St::MarginTop => rem(0.5),
         St::Padding => ".75rem 1.25rem",
         St::Color => "#ff0303",
         St::Background => "rgba(220, 17, 1, 0.3)",
         St::Border => "1px solid rgba(241, 6, 6, 0.81)",
         St::BorderRadius => "0.25rem",
         St::TextShadow => "1px 1px 1px rgba(0,0,0,0.3)",
+        St::MarginTop => rem(-2)
+        
     };
+    let mut s_toast_trans = s_toast.clone();
+    s_toast_trans.add(St::Transition, "margin-top 1s linear");
+    s_toast_trans.add(St::MarginTop, rem(1));
+
     let s_title= style! {
         St::MarginLeft => rem(0.7);
         St::MarginRight => rem(0.7);
     };
     nodes![
-        if model.toast.is_visible && 
-            model.toast.title.is_some() && 
-            model.toast.content.is_some() {
-                div![
-                    class!("toast"),
-                    s_toast,
-                    i![
-                        class!("far fa-times-circle"),
-                    ],
-                    strong![
-                        s_title,
-                        model.toast.clone().title.unwrap()
-                    ],
-                    model.toast.clone().content.unwrap()
+        div![
+            class!("toast"),
+            match model.toast.is_visible {
+                true => s_toast_trans,
+                false => s_toast
+            },
+            i![
+                class!("far fa-times-circle"),
+            ],
+            if let Some(title) = model.toast.title.clone() {
+                strong![
+                    s_title,
+                    title
                 ]
-        } else {
-            empty![]
-        },
+                //model.toast.clone().content.unwrap()
+            } else {
+                empty![]
+            },
+            if let Some(content) = model.toast.content.clone() {
+                content
+            } else {
+                String::new()
+            }
+
+        ]
     ]
 }
