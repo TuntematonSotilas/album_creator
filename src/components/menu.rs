576f0b3f1eeed86a9cc6e8be55214170d47f9613
G.Batault <g.batault@citegestion.fr>
2020-10-22 13:39:57 +0200

    menu

--- src/components/menu.rs
+++ src/components/menu.rs
@@ -20,20 +20,45 @@ pub fn update(_msg: Msg, _model: &mut Model, _orders: &mut impl Orders<Msg>) {}
 // ------------
 
 pub fn view(_model: &Model) -> Vec<Node<Msg>> {
+    let s_nav = style! {
+        St::Display => "flex",
+    };
     let s_item = style! {
+        St::Width => rem(5),
+        St::Height => rem(5),
+        St::Margin => rem(1),
         St::BorderRadius => percent(100),
-        St::Width => rem(20),
-        St::Height => rem(20),
+        St::Color => "white",
+        St::FontSize => rem(1.5),
+        St::TextAlign => "center",
+        St::LineHeight => rem(5),
+        St::TextShadow => "1px 1px 0 rgba(0, 0, 0, 0.12)",
+        St::BoxShadow => "3px 3px 0 0 rgba(0, 0, 0, 0.14)"
+    };
+    let s_blue = style! {
+        St::Background => "#00587a"
+    };
+    let s_green = style! {
+        St::Background => "#008891"
     };
     nodes![
         nav![
+            s_nav,
             a![
+                class!("menu__item menu__item--blue"),
+                s_blue,
+                s_item.clone(),
+                attrs! { At::Href => String::new() },
                 i![
-                    s_item.clone(),
                     class!("fa fa-book-open"),
                 ],
+            ],
+            a![
+                class!("menu__item menu__item--green"),
+                s_green,
+                s_item.clone(),
+                attrs! { At::Href => String::new() },
                 i![
-                    s_item.clone(),
                     class!("fa fa-plus"),
                 ],
             ],
