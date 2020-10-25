3aabd793625c4b50178c27ec7022604b9ef63772
TuntematonSotilas <gatobael@gmail.com>
2020-10-25 11:53:48 +0100

    header

--- src/components/header.rs
+++ src/components/header.rs
@@ -23,7 +23,7 @@ pub fn view(_model: &Model) -> Vec<Node<Msg>> {
 	let s_header = style! {
 		St::Padding => rem(0.5),
 		St::TextAlign => "center",
-		St::Background => "#4474ad",
+		St::Background => "radial-gradient(circle at top left, rgba(130, 130, 130, 0.5) 0%, rgba(0,0,0,0) 100%), #4474ad",
 		St::BoxShadow => "0 0px 2px rgba(0, 0, 0, 0.5)",
 		St::FontSize => rem(1),
 		St::Color => "white",
