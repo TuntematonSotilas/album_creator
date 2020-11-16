e943e188cc4a717ae757da824749b45644ed9d46
G.Batault <g.batault@citegestion.fr>
2020-11-16 17:56:10 +0100

    album-detail

--- src/components/album.rs
+++ src/components/album.rs
@@ -1,12 +1,34 @@
 use seed::{self, prelude::*, *};
 
+use crate::utils::{
+	vars::API_URI,
+	request::get_auth
+};
+
 // ------------
 //     Model
 // -----------
 
 #[derive(Default)]
-pub struct Model {}
+pub struct Model {
+	album: Option<Album>,
+}
 
+#[derive(serde::Deserialize)]
+pub struct Album {
+	info: Info,
+	pictures: Vec<Pictures>,
+}
+
+#[derive(serde::Deserialize)]
+pub struct Info {
+    name: String,
+}
+
+#[derive(serde::Deserialize)]
+pub struct Pictures {
+    frid: String,
+}
 
 // ------------
 //    Update
@@ -14,12 +36,37 @@ pub struct Model {}
 
 pub enum Msg {
 	Show(Option<String>),
+	Recieved(Option<Album>),
 }
 
-pub fn update(msg: Msg, _model: &mut Model, _orders: &mut impl Orders<Msg>) {
+pub fn update(msg: Msg, model: &mut Model, orders: &mut impl Orders<Msg>) {
 	match msg {
 		Msg::Show(id_url) => {
-			log!(id_url);
+			orders.skip(); // No need to rerender
+			orders.perform_cmd(async {
+				let mut opt: Option<Album> = None;
+
+				if let Some(id) = id_url {
+					let uri = format!("{0}get-album-detail?id={1}", API_URI, id);
+					let request = Request::new(uri)
+						.method(Method::Get)
+						.header(Header::authorization(get_auth()));
+					let response_res = fetch(request).await;
+
+					if let Ok(response) = response_res {
+						if let Ok(resp_ok) = response.check_status() {
+							let albums_res = resp_ok.json::<Album>().await;
+							if let Ok(albums) = albums_res {
+								opt = Some(albums);
+							}
+						}
+					}
+				}
+				Msg::Recieved(opt)
+			});
+		},
+		Msg::Recieved(opt) => {
+			model.album = opt;
 		},
 	}
 }
@@ -28,8 +75,44 @@ pub fn update(msg: Msg, _model: &mut Model, _orders: &mut impl Orders<Msg>) {
 //     View
 // ------------
 
-pub fn view(_model: &Model) -> Vec<Node<Msg>> {
+pub fn view(model: &Model) -> Vec<Node<Msg>> {
+	let s_album = style! {
+		St::Margin => rem(1),
+		St::Display => "flex",
+		St::FlexDirection => "column",
+		St::JustifyContent => "center",
+	};
+	let s_title = style! {
+		St::TextAlign => "center",
+		St::FontSize => rem(2),
+		St::LetterSpacing => rem(0.1),
+		St::TextShadow => "0 0 1rem rgba(0,0,0,0.3)",
+	};
+	let s_no_photos = style! {
+		St::TextAlign => "center",
+	};
 	nodes![
-		span!["album"],
+		match &model.album {
+			Some(album) => div![
+				s_album,
+				h1![
+					s_title,
+					&album.info.name
+				],
+				/*span![
+					s_no_photos,
+					"No photos"
+				],*/
+				album.pictures.iter().map(|picture| a![
+					img![
+						attrs! { 
+							At::Alt => picture.frid 
+						}
+					]
+				])
+
+			],
+			None => empty![],
+		}
 	]
 }
