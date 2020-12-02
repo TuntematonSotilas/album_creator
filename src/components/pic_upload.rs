def5e444342de3ded28808b1d19b7db8b69c647d
G.Batault <g.batault@citegestion.fr>
2020-12-02 16:58:40 +0100

    css

--- src/components/pic_upload.rs
+++ src/components/pic_upload.rs
@@ -1,5 +1,7 @@
 use seed::{self, prelude::*, *};
 
+use crate::utils::style::s_button;
+
 // ------------
 //     Model
 // -----------
@@ -20,28 +22,18 @@ pub fn update(_msg: Msg, _model: &mut Model, _orders: &mut impl Orders<Msg>) {}
 // ------------
 
 pub fn view(_model: &Model) -> Vec<Node<Msg>> {
-    let s_file_btn = style! {
-		St::Width => rem(5),
-        St::Height => rem(5),
-		St::Margin => rem(1),
-		St::BorderRadius => rem(0.2),
-		St::FontSize => rem(2),
-		St::TextAlign => "center",
-		St::LineHeight => rem(5),
-		St::Color => "#3f46ac",
-		St::Background => "rgba(0, 0, 0, 0.2)",
-		St::TextShadow => "1px 1px 1px rgba(0,0,0,0.3)",
-	};
 	let s_input_file = style! {
 		St::Display => "none",
 	};
+	let s_btn = style! {
+		St::MarginTop => rem(1),
+	};
 	nodes![
 		label![
-            s_file_btn,
-            C!("edit_ablum__file_btn"),
-            i![
-                C!("fa fa-plus"),
-            ],
+			s_btn,
+			s_button(),
+			C!("button"),
+            "Upload",
             input![
                 s_input_file,
                 attrs! {
