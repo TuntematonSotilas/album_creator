f8250ef7f06fa2fccc93c1c4f2ce0b4046c04f51
TuntematonSotilas <gatobael@gmail.com>
2020-12-04 20:09:08 +0100

    upload

--- src/components/pic_upload.rs
+++ src/components/pic_upload.rs
@@ -1,6 +1,8 @@
 use seed::{self, prelude::*, *};
+use web_sys::{FileList, FileReader, Blob};
 
 use crate::utils::style::s_button;
+
 
 // ------------
 //     Model
@@ -13,9 +15,28 @@ pub struct Model {}
 //    Update
 // ------------
 
-pub enum Msg {}
+pub enum Msg {
+	FilesChanged(Option<FileList>),
+}
 
-pub fn update(_msg: Msg, _model: &mut Model, _orders: &mut impl Orders<Msg>) {}
+pub fn update(msg: Msg, _model: &mut Model, orders: &mut impl Orders<Msg>) {
+	match msg {
+		Msg::FilesChanged(files_opt) => {
+			if let Some(files) = files_opt {
+				let file_opt = files.get(0);
+				if let Some(file) = file_opt {
+					orders.perform_cmd(async move {
+						let data_res = gloo_file::futures::read_as_data_url(&gloo_file::Blob::from(file.clone())).await;
+						if let Ok(data) = data_res {
+							log!(data);
+						}
+					});
+					
+				}
+			}
+		}
+	}
+}
 
 // ------------
 //     View
@@ -33,13 +54,22 @@ pub fn view(_model: &Model) -> Vec<Node<Msg>> {
 			s_btn,
 			s_button(),
 			C!("button"),
-            "Upload",
+            "Add pictures",
             input![
                 s_input_file,
                 attrs! {
                     At::Type => "file",
-                    At::Accept => "image/*",
-                },
+					At::Accept => "image/*",
+					At::Multiple => "false"
+				},
+				ev(Ev::Change, |event| {
+                    let files = event
+                        .target()
+                        .and_then(|target| target.dyn_into::<web_sys::HtmlInputElement>().ok())
+                        .and_then(|file_input| file_input.files());
+
+                    Msg::FilesChanged(files)
+                }),
             ],
         ],
 	]
