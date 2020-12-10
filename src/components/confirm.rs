20b8a4da4d0627cde264dc0195774282e28fdb2e
G.Batault <g.batault@citegestion.fr>
2020-12-10 18:35:39 +0100

    confirm

--- src/components/confirm.rs
+++ src/components/confirm.rs
@@ -35,11 +35,21 @@ pub fn view(model: &Model) -> Vec<Node<Msg>> {
     let s_confirm = style!{
         St::Position => "fixed",
         St::Background => "rgba(0, 0, 0, 0.5)",
-        St::Transition => "opacity 400ms ease-out",
+        St::Transition => "opacity 600ms ease-out",
         St::Width => percent(100),
         St::Height => percent(100),
         St::AlignItems => "center",
         St::JustifyContent => "center",
+        St::TextShadow => "1px 1px 1px rgba(0,0,0,0.3)",
+    };
+    let s_modal = style! {
+        St::Display => "flex",
+        St::AlignItems => "center",
+        St::JustifyContent => "center",
+        St::Background => "radial-gradient(circle at top right, #c3e6e8 -30%, #008790 100%)",
+        St::BorderRadius => rem(0.2),
+        St::Width => rem(15),
+        St::Height => rem(8),
     };
 	let s_anim = match model.is_visible {
 		true => style! {
@@ -54,8 +64,11 @@ pub fn view(model: &Model) -> Vec<Node<Msg>> {
     nodes![
         div![
 			s_confirm,
-			s_anim,
-            span![&model.message],
+            s_anim,
+            div![
+                s_modal,
+                span![&model.message],
+            ],
         ]
     ]
 }
