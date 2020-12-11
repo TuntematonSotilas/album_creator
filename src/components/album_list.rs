1bddb4baf0fb0f497f912933d6555cb937f7c5f1
TuntematonSotilas <gatobael@gmail.com>
2020-12-11 18:28:16 +0100

    remove

--- src/components/album_list.rs
+++ src/components/album_list.rs
@@ -87,7 +87,17 @@ pub fn update(msg: Msg, model: &mut Model, orders: &mut impl Orders<Msg>) {
 			}
 		},
 		Msg::DeleteResp(is_ok) => {
-			log!("DeleteResp", is_ok);
+			if is_ok {
+				if let Some(album) = &mut model.albums {
+					if let Some(frid) = &model.frid_to_delete {
+						let position = album.into_iter()
+							.position(|a| a.frid == *frid);
+						if let Some(pos) = position {
+							album.remove(pos);
+						}	
+					}
+				}
+			}
 		},
 	}
 }
